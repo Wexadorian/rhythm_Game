40e0fe6f6bc22adaa1ca9ab31df975ff74f45d45
Wexadorian <ebenwagnernz@gmail.com>
2025-08-29 14:58:25 +1200

    buttons changed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
     }
 
+    public void CloseGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
     public void ToMainMenu()
     {
         SceneManager.LoadSceneAsync(0);
